652d513df6f25ad248d2c265883798fe96cfc7c0
Gabe Cederberg <gabriel.cederberg@gmail.com>
2021-02-28 14:45:28 -0600

    Daily update

--- analysis_app/app.R
+++ analysis_app/app.R
@@ -12,20 +12,20 @@ library(scales)
 
 # Set date
 
-end_date <- "Feb. 26, 2021"
+end_date <- "Feb. 28, 2021"
 
 # Load data
 
-map_data <- read_rds("data_files/map_data2021-02-26.rds")
-county_map_data <- read_rds("data_files/county_map_data2021-02-26.rds")
+map_data <- read_rds("data_files/map_data2021-02-28.rds")
+county_map_data <- read_rds("data_files/county_map_data2021-02-28.rds")
 
-chart_data <- read_rds("data_files/case_chart_data2021-02-26.rds")
-vax_chart_data <- read_rds("data_files/vax_chart_data2021-02-26.rds")
+chart_data <- read_rds("data_files/case_chart_data2021-02-28.rds")
+vax_chart_data <- read_rds("data_files/vax_chart_data2021-02-28.rds")
 
-awesome <- read_rds("data_files/awesome2021-02-26.rds")
-cool <- read_rds("data_files/cool2021-02-26.rds")
+awesome <- read_rds("data_files/awesome2021-02-28.rds")
+cool <- read_rds("data_files/cool2021-02-28.rds")
 
-hosp_figure <- read_rds("data_files/ctp2021-02-26.rds")
+hosp_figure <- read_rds("data_files/ctp2021-02-28.rds")
 
 geo <- read_rds("data_files/geo_data.rds")
 county_geo <- read_rds("data_files/county_geo_data.rds")
